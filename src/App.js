134e71d51f04db5cef02c072138be3be2f78eee3
mabdurrakib <mabdurrakib.dev@gmail.com>
2021-12-05 00:28:53 +0600

    showing country from an api

--- src/App.js
+++ src/App.js
@@ -5,13 +5,23 @@ import { useEffect, useState } from 'react';
 function App() {
   const [countries, setCountries] = useState([]);
   useEffect(()=>{
-    fetch('https://restcountries.com/v3.1/all')
+    fetch('https://restcountries.com/v2/all')
     .then(res => res.json())
-    .then(data => setCountries(data))
-  },[])
+    .then(data => {
+      setCountries(data);
+      const names = data.map(country => country.name);
+      console.log(names);
+      })
+    .catch(error=> console.log(error))
+  }, [])
+
+
   return (
     <div className="App">
       <h1> Countires: {countries.length}</h1>
+      <ul>
+        {countries.map(country => <li>{country.name} </li>)}
+      </ul>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
