206e1bbed3ec6bcea36eba320dcfe7741038a061
mabdurrakib <mabdurrakib.dev@gmail.com>
2021-12-04 21:59:42 +0600

    use effect and useState declared

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => setCountries(data))
+  },[])
   return (
     <div className="App">
+      <h1> Countires: {countries.length}</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
